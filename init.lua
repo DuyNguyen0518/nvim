540cf9681a90cc9d5fa52c5a16713b852fdb6014
DuyNguyen0518 <phuchaiduynguyen@gmail.com>
2026-02-16 19:48:20 +1100

    Lazyvim

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
--- Show line numbers
-vim.opt.number = true
-
--- Tab configuration: 4 spaces
-vim.opt.tabstop = 4      -- Visual width of a tab
-vim.opt.shiftwidth = 4   -- Width for auto-indentation
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.softtabstop = 4  -- Makes backspace treat 4 spaces like a tab
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")

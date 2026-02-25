dc9e569d106fb8c39433b4a604fe3defc0a19152
DuyNguyen0518 <phuchaiduynguyen@gmail.com>
2026-02-25 14:19:56 +0000

    Add transparent background

--- init.lua
+++ init.lua
@@ -6,3 +6,10 @@ vim.opt.tabstop = 4      -- Visual width of a tab
 vim.opt.shiftwidth = 4   -- Width for auto-indentation
 vim.opt.expandtab = true -- Convert tabs to spaces
 vim.opt.softtabstop = 4  -- Makes backspace treat 4 spaces like a tab
+
+-- Enable transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+

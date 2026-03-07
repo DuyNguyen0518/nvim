95f1ff030fc1d4437b09b21309ee8a5a73b4beb1
DuyNguyen0518 <phuchaiduynguyen@gmail.com>
2026-03-07 15:01:45 +0000

    Add clipboard

--- init.lua
+++ init.lua
@@ -13,3 +13,5 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
 
+-- Set clipboard
+vim.opt.clipboard = "unnamedplus"
